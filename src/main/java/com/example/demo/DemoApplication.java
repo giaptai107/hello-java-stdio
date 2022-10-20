f624a8e987cc1a433910e6dc4be4b6b6d34fc58f
Tài Trương <giaptai170@gmail.com>
2022-10-20 09:18:11 +0700

    Commit lần 8

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String helloJava() {
-		return "Hello Java lần 7";
+		return "Hello Java lần 8";
 	}
 
 	public static void main(String[] args) {
